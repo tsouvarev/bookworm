21ee2189741102d720635f0f4a171d406041d747
Ivan Tsuvarev <i.tsuvarev@tinkoff.ru>
2021-01-07 16:49:25 +0300

    Reformat js, css and html files to indent of 2 spaces

--- static/app.js
+++ static/app.js
@@ -1,33 +1,33 @@
 new Vue({
-    el: "#app",
-    data: {
-        books: [],
-        isLoading: true,
-        new_book: {},
-        new_book_errors: {}
+  el: "#app",
+  data: {
+    books: [],
+    isLoading: true,
+    new_book: {},
+    new_book_errors: {}
+  },
+  delimiters: ["[[", "]]"],
+  mounted() {
+    axios.get('/books/').then(response => {
+      this.books = response.data;
+      this.isLoading = false;
+    });
+  },
+  methods: {
+    create_book: function () {
+      axios.post(`/books/`, this.new_book).then(response => {
+        this.books.unshift(response.data);
+        this.new_book_errors = {};
+      }).catch(error => {
+        this.new_book_errors = error.response.data.errors;
+      })
     },
-    delimiters: ["[[", "]]"],
-    mounted() {
-        axios.get('/books/').then(response => {
-            this.books = response.data;
-            this.isLoading = false;
-        });
-    },
-    methods: {
-        create_book: function () {
-            axios.post(`/books/`, this.new_book).then(response => {
-                this.books.unshift(response.data);
-                this.new_book_errors = {};
-            }).catch(error => {
-                this.new_book_errors = error.response.data.errors;
-            })
-        },
-        delete_book: function (book_id) {
-            axios.delete(`/books/${book_id}/`).then(response => {
-                this.books = this.books.filter(value => value.id != book_id);
-            })
-        }
+    delete_book: function (book_id) {
+      axios.delete(`/books/${book_id}/`).then(response => {
+        this.books = this.books.filter(value => value.id != book_id);
+      })
     }
+  }
 });
 
 let csrf_meta_tag = document.querySelector('meta[name="csrf-token"]');
