791be9ad58f3b42fd27777783b3db9bf66ebeae4
Ivan Tsouvarev <tsouvarev@mail.ru>
2023-10-23 07:58:31 +0300

    Show stats for helpful and english books

--- app/documents.py
+++ app/documents.py
@@ -1,13 +1,16 @@
 import mongoengine
+import pendulum
 
-from .utils import now
+
+def _now():
+    return pendulum.now('Europe/Moscow')
 
 
 class Book(mongoengine.Document):
     author = mongoengine.StringField(required=True)
     title = mongoengine.StringField(required=True)
     pages_number = mongoengine.IntField(required=True)
-    date_start = mongoengine.DateField(default=now)
+    date_start = mongoengine.DateField(default=_now)
     date_end = mongoengine.DateField()
     comment = mongoengine.StringField()
     rating = mongoengine.IntField()
--- app/utils.py
+++ app/utils.py
@@ -1,9 +1,12 @@
-from collections import defaultdict
+import re
 from collections.abc import Iterable
 from dataclasses import dataclass
-from itertools import starmap
+from itertools import groupby
+from typing import Optional
 
-import pendulum
+from whatever import that
+
+from .documents import Book
 
 READABLE_MONTH_NAMES = [
     'Январь',
@@ -22,36 +25,77 @@ READABLE_MONTH_NAMES = [
 
 
 @dataclass
-class GroupedBooks:
+class BooksStats:
+    books: list
+    total_stats: Optional['BooksStats'] = None
+
+    @property
+    def count(self):
+        return len(self.books)
+
+    @property
+    def percent(self):
+        if self.total_stats is None:
+            raise NotImplementedError
+        return round(self.count / self.total_stats.count * 100)
+
+    @property
+    def pages(self):
+        return sum(book.pages_number for book in self.books)
+
+    @property
+    def pages_percent(self):
+        if self.total_stats is None:
+            raise NotImplementedError
+        return round(self.pages / self.total_stats.pages * 100)
+
+
+@dataclass
+class BooksByMonth:
     month: int
-    data: list
+    stats: BooksStats
+
+
+@dataclass
+class BooksByYear:
+    year: int
+    books: list[Book]
 
     @property
-    def pages_sum(self):
-        return sum(book.pages_number for book in self.data)
+    def books_by_month(self) -> Iterable[BooksByMonth]:
+        grouped_by_month = groupby(self.books, that.date_start.month)
+        for month, books in grouped_by_month:
+            yield BooksByMonth(month, BooksStats(list(books)))
 
     @property
-    def books_number(self):
-        return len(self.data)
+    def stats(self):
+        return BooksStats(self.books)
 
+    @property
+    def helpful_stats(self):
+        minimal_helpful_rating = 3
+        helpful_books = [
+            book
+            for book in self.books
+            if book.rating and book.rating > minimal_helpful_rating
+        ]
+        return BooksStats(helpful_books, total_stats=self.stats)
 
-def now():
-    return pendulum.now('Europe/Moscow')
+    @property
+    def english_stats(self):
+        english_books = [
+            book
+            for book in self.books
+            if re.match('^[a-zA-Z0-9]+$', re.sub(r'[^\w]', '', book.title))
+        ]
+        return BooksStats(english_books, total_stats=self.stats)
 
 
 def get_readable_month_name(month_number):
     return READABLE_MONTH_NAMES[month_number - 1]
 
 
-def group_books(books):
-    grouped = defaultdict(lambda: defaultdict(list))
-    for book in books:
-        grouped[book.date_start.year][book.date_start.month].append(book)
-
-    for year, grouped_by_month in grouped.items():
-        aggregated_books = list(_aggregate_books(grouped_by_month))
-        yield year, aggregated_books
-
-
-def _aggregate_books(grouped_by_month: dict[int, list]) -> Iterable[GroupedBooks]:
-    yield from starmap(GroupedBooks, grouped_by_month.items())
+def group_books(books: list[Book]) -> Iterable[BooksByYear]:
+    grouped_by_year = groupby(books, that.date_start.year)
+    for year, books in grouped_by_year:
+        yield BooksByYear(year, list(books))
