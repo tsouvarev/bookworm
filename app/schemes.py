499c40f47768f0562c661093f501ab1d7acdf79c
Ivan Tsouvarev <tsouvarev@mail.ru>
2024-06-22 11:28:41 +0300

    Format dates in book list more humanly readable

--- app/schemes.py
+++ app/schemes.py
@@ -1,4 +1,9 @@
+from datetime import date
+
 from marshmallow import Schema, fields
+from pendulum import now
+
+from app.documents import Book
 
 
 class BookSchema(Schema):
@@ -6,10 +11,23 @@ class BookSchema(Schema):
     author = fields.String()
     title = fields.String()
     pages_number = fields.Integer()
-    date_start = fields.Date()
-    date_end = fields.Date()
+    date_start = fields.Method('get_date_start')
+    date_end = fields.Method('get_date_end')
     comment = fields.String()
     rating = fields.Integer()
+
+    def get_date_start(self, book: Book) -> str:
+        return self._format_date(book.date_start)
+
+    def get_date_end(self, book: Book) -> str | None:
+        if not book.date_end:
+            return None
+        return self._format_date(book.date_end)
+
+    def _format_date(self, d: date) -> str:
+        if d.year == now().year:
+            return d.strftime('%-d %b')
+        return d.strftime('%-d %b %Y')
 
 
 class AddBoookSchema(Schema):
